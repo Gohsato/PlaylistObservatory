02ce8cacd3e3d786f436c6b6fdc1eeec54f6edeb
Goh Sato <goh.sato@outlook.com>
2020-09-14 14:18:51 -0700

    Updates

---
+++ precache-manifest.b023bfd7a685882c6f12a37c7a825f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3e092d87f60f1415581ce78681a64b",
+    "url": "/PlaylistObservatory/index.html"
+  },
+  {
+    "revision": "4156460dc80e905cdf81",
+    "url": "/PlaylistObservatory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d653650f58508b79e3fd",
+    "url": "/PlaylistObservatory/static/css/main.cadaca72.chunk.css"
+  },
+  {
+    "revision": "4156460dc80e905cdf81",
+    "url": "/PlaylistObservatory/static/js/2.958aa31c.chunk.js"
+  },
+  {
+    "revision": "e82971700148a1b13f338a6f1497f7ac",
+    "url": "/PlaylistObservatory/static/js/2.958aa31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d653650f58508b79e3fd",
+    "url": "/PlaylistObservatory/static/js/main.b56c102f.chunk.js"
+  },
+  {
+    "revision": "c9384e6ddd70d507bbe4",
+    "url": "/PlaylistObservatory/static/js/runtime-main.7cd6c4aa.js"
+  }
+]);
